9e2ebea674186265c06deb11ee4ecd4b78af6658
Hayden Kiltoff <hayden060@gmail.com>
2022-05-10 17:59:38 -0700

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# note-taker
+
+## Note taker
+
+
+## License(s)
+[![License: ](https://img.shields.io/badge/License--blue.svg)](https://opensource.org/licenses/MIT)
+
+
+## table of contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Credits](#credits)
+5. [License](#license)
+6. [Questions](#questions)
+        
+        
+        
+## Description
+This project allows you to type out multiple notes and and save them for later use
+
+this project was built as a deployable note taking application 
+
+this application allows the USER to type out their notes and save them for later
+
+this project allowed me to understand the app.get/app.use and create coorisponding tables
+        
+        
+## Installation
+the use this application from a clone, you must install npm, express, inquirer
+
+
+        
+        
+## Usage
+Use the localhost and generate notes that save when the application is open
+
+![note-taker start page](./note-taker/assets/Untitled_%20May%2010%2C%202022%205_57%20PM.gif)
+        
+## Credits
+Hayden Kiltoff
+
+no third party assets were used
+        
+        
+
+## Questions
+You can access my github repository from here https://github.com/hayden1773
+        
+You may also forward an email for more information or questions to hayden060@gmail.com
+        
+## Features
+
+        
+        
+## Tests 
--- note-taker/Develop/server.js
+++ note-taker/Develop/server.js
@@ -1,29 +1,61 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
+// const api = require('./public/assets/js/index.js');
 const PORT = process.env.port || 3001;
 const app = express();
+const fs = require ('fs');
+
+
 
 
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes'))
-);
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/api/notes',  (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+     res.json(JSON.parse(data)); 
+    }
+  });
+}
+)
+
+app.post('/api/notes', (req,res)=> {
+  fs.readFile('./db/db.json', 'utf8', (err,data) =>{
+    if (err) {
+      console.log(err)
+    } else {
+      let newNote = req.body
+      let offNotes = JSON.parse(data)
+      console.log(newNote);
+      console.log(offNotes);
+      offNotes.push(newNote)
+      fs.writeFile('./db/db.json', JSON.stringify(offNotes),(err,data)=> {
+        if (err) {
+          console.error(err);
+        } else {
+         res.json(newNote); 
+        }
+      });
+    }
+  })
+})
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
